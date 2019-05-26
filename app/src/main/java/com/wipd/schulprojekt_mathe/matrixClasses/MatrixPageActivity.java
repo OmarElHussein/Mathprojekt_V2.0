0260d7dd010df3acf0ecc239e19b33b971aa9051
OmarElHussein <omarelhussein9@gmail.com>
2019-05-26 15:56:31 +0200

    Matrix

--- app/src/main/java/com/wipd/schulprojekt_mathe/matrixClasses/MatrixPageActivity.java
+++ app/src/main/java/com/wipd/schulprojekt_mathe/matrixClasses/MatrixPageActivity.java
@@ -1,7 +1,9 @@
 package com.wipd.schulprojekt_mathe.matrixClasses;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.wipd.schulprojekt_mathe.R;
 
@@ -11,5 +13,17 @@ public class MatrixPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix_page);
+    }
+
+    public void oeffneMultiplikationMitEinerZahl(View view) {
+        Intent intent = new Intent(this, MatrixRechnerActivity.class);
+        startActivity(intent);
+    }
+    public void oeffneAddidtionZweierMatrizen (View view) {
+        Intent intent = new Intent(this, MatrixRechnerActivity.class);
+        startActivity(intent);
+    }
+    public void oeffeneMultiplikationZweierMatrizen (View view) {
+        Intent intent = new Intent(this, MatrixRechnerActivity.class);
     }
 }
