51f320901b04eff8b39ff9b342e349f13c3edfee
OmarElHussein <omarelhussein9@gmail.com>
2019-05-27 12:18:33 +0200

    für Omar Vektorrechnung

---
+++ app/src/main/java/com/wipd/schulprojekt_mathe/VektorrechnungClasses.java
@@ -0,0 +1,24 @@
+package com.wipd.schulprojekt_mathe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class VektorrechnungClasses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vektorrechnung_classes);
+
+
+    }
+    private editTexta1;
+
+    a1 = findViewById(R.id.a1);
+
+
+
+
+
+}
