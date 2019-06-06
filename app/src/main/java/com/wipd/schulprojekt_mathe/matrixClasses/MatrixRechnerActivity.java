c4d6093bd777b0c7e4b3d6dbd8e92c9d73a72436
Omar El Hussein <48603543+OmarElHussein@users.noreply.github.com>
2019-06-06 19:09:15 +0200

    Update

--- app/src/main/java/com/wipd/schulprojekt_mathe/matrixClasses/MatrixRechnerActivity.java
+++ app/src/main/java/com/wipd/schulprojekt_mathe/matrixClasses/MatrixRechnerActivity.java
@@ -153,7 +153,6 @@ public class MatrixRechnerActivity extends AppCompatActivity {
 
                 editText_matrix_werte.requestFocus();
                 matrixSecondConstraint.setVisibility(View.VISIBLE);
-
             }
 
         } else {
